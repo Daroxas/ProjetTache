0a6ac85e98e0ebd340ff404e92567c2e4b8d9c82
Trystan <trystan.julien@viacesi.fr>
2023-11-19 14:54:12 +0100

    Constructeur TachePrio fini

--- TachePrio.cpp
+++ TachePrio.cpp
@@ -4,7 +4,13 @@
 
 TachePrio :: TachePrio(str nom, str description, int priorite, str avancement, int delai, str deadline)
 {
-    
+    cout << GRN "Tâche urgente" << RED << nom << GRN " créée avec succés" << endl << endl;
+    this->nom = nom;
+    this->descritpion = description;
+    this->priorite = priorite;
+    this->avancement = avancement;
+    this->delai = delai;
+    this->deadline = deadline;
 }
 
 str TachePrio :: GetDeadline()
